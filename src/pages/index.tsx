814f6db6af1d1c2b76b92924564ef1ef900c0170
Pedro Gallardo <pedrogallardo7011@gmail.com>
2025-03-30 18:33:30 -0300

    fix(links): fixes docs internal links

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import Layout from "@theme/Layout";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import Link from "@docusaurus/Link";
 import wordsUpperCase from "@site/utils/wordsUpperCase";
+import useBaseUrl from "@docusaurus/useBaseUrl";
 
 type customFields = {
   [x: string]: string;
@@ -18,7 +19,7 @@ export default function Home() {
       <div style={{ textAlign: "center", padding: "2rem" }}>
         <h1>{wordsUpperCase(projectName)}</h1>
         <p>Lorem ipsum dolor sit amet.</p>
-        <Link to={`/docs/${versionPath}`}>Get started</Link>
+        <Link to={useBaseUrl(`/docs/${versionPath}`)}>Get started</Link>
       </div>
     </Layout>
   );
