31e34c825bfe745b32b3c3bee29739e46e7fc9c4
Pedro Gallardo <pedrogallardo7011@gmail.com>
2025-04-10 15:31:05 -0300

    feat(sass-searchbar): adds sass and local search plugin

--- config/constants.ts
+++ config/constants.ts
@@ -1,3 +1,21 @@
+import { getVersionsMetadata, semverRegexp } from "../utils/versions-metadata";
+import docVersions from "../versions.json";
+
 export const projectName = "project-name";
 export const organizationName = "drusco";
 export const tagline = "Project description";
+
+export const versions: string[] = docVersions;
+export const versionsMetadata = getVersionsMetadata(versions);
+
+// At least one version should exist for docs to work
+if (!versions.length) {
+  versions.push("current");
+  versionsMetadata.current = {
+    label: "development",
+    path: "",
+  };
+}
+
+export const lastVersion =
+  versions.find((version) => semverRegexp.test(version)) || versions[0];
