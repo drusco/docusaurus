574aa27f8787dc9414313c20ec4b7969f0564218
Pedro Gallardo <pedrogallardo7011@gmail.com>
2025-04-10 16:24:47 -0300

    fix(versions): updates the current version metadata when the only version is the current dev version

--- config/constants.ts
+++ config/constants.ts
@@ -11,11 +11,15 @@ export const versionsMetadata = getVersionsMetadata(versions);
 // At least one version should exist for docs to work
 if (!versions.length) {
   versions.push("current");
+}
+
+export const lastVersion =
+  versions.find((version) => semverRegexp.test(version)) || versions[0];
+
+// Overwrite version metadata for current version
+if (lastVersion === "current") {
   versionsMetadata.current = {
     label: "development",
     path: "",
   };
 }
-
-export const lastVersion =
-  versions.find((version) => semverRegexp.test(version)) || versions[0];
